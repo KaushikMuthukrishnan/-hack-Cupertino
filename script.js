9a32965d931dfd403720bc57a8d759e6349a18d8
KaushikMuthukrishnan <kaushikmn113@gmail.com>
2020-07-24 17:08:18 -0700

    Work Day 5a

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 
 //Mobile Sidebar Functionality =>//
+
+
+
 function collapse(element) {
 	element.style.transitionProperty = "height";
 	element.style.transitionDuration = "300ms";
 	let sbHeight = element.scrollHeight;
 
-	requestAnimationFrame(function () {
+	requestAnimationFrame(() => {
 		element.style.height = sbHeight + "px";
 		
-		requestAnimationFrame( function() { 
+		requestAnimationFrame( () => { 
 			element.style.height = 0 + "px";
 		});
 	});
@@ -17,10 +20,11 @@ function collapse(element) {
 	element.style.visibility = "hidden";
 };
 
+
 function expand(element) {
 	let sbHeight = element.scrollHeight;
 	element.style.height = sbHeight + "px";
-	element.addEventListener("transitioned", function(event) {
+	element.addEventListener("transitioned", event => {
 		element.removeEventListener("transitioned", arguments.callee);
 		element.style.height = null;
 	});
@@ -29,21 +33,32 @@ function expand(element) {
 	element.style.visibility = "visible";
 };
 
+
+
 let sidebar = document.querySelector("#collapsible-sidebar-items");
 
 $(collapse(sidebar));
 
-document.querySelector("#collapsible-sidebar-toggle").addEventListener("click", function(event) {
+document.querySelector("#collapsible-sidebar-toggle").addEventListener("click", (event) => {
 		
-		if(sidebar.getAttribute("data-collapsed") === "true") {
-			expand(sidebar);
-			sidebar.setAttribute("data-collapsed", "false");
-		} else {
-			collapse(sidebar);
-		}
+	if(sidebar.getAttribute("data-collapsed") === "true") {
+		expand(sidebar);
+	} else {
+		collapse(sidebar);
+	}
+});
+
+sidebar.addEventListener("click", event => {
+	collapse(sidebar);
 });
 
 //<=Mobile Sidebar Functionality//
 
+
+
+
+if (window.innerWidth > 768) {
+	$("#CSBhr").hide();
+} 
 
 		
